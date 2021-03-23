419b458db33bcd505e45ce251de933607c4ef04b
Yazan Baker <yazanbaker94@hotmail.com>
2021-03-23 11:22:07 +0200

    fixed ceremony and styles and about us

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
 
-<<<<<<< HEAD
-=======
-
 //  start of function  of sliding images in ceremony page added by Hamza
 
 let myIndex = 0;
@@ -20,73 +17,65 @@ function occasions() {
 }
 // end of function  of sliding images in ceremony page added by Hamza
 
+// THIS IS OUR OVERLAY RENDER FUNCTION AND LOCALSTORAGE
 
 
 
-//add to cart part
-let carts=document.querySelectorAll('.add-cart');
-let products=[
-  {
-    name:'Flower ',
-    tag:'Grey',
-    price:15,
-    inCart:0
+function renderOverlay(){
+  let text = document.getElementById('text');
+  let overlay = document.getElementById('overlay').style.display = "block";
+  
+  text.setAttribute('style', 'white-space: pre;');
+  text.textContent = 'Hello ' + firstNameValue + ' ' + lastNameValue + "!\r\n We are happy that you chose us! \r\n We will be contacting you as soon as possible!" ;
+  
 
-  },
-  {
-    name:'Blue',
-    tag:'Blue flower',
-    price:20,
-    inCart:0
 
-  },
-  {
-    name:'Yellow',
-    tag:'Yellow',
-    price:10,
-    inCart:0
+  let exitButton = document.getElementById('exitButton');
+  exitButton.textContent = "Exit";
+  exitButton.addEventListener('click', clickHandler);
 
-  },
-  {
-    name:'Tulip ',
-    tag:'Tulip ',
-    price:25,
-    inCart:0
-
-  }
-];
-for(let i=0;i<carts.length;i++){
-  carts[i].addEventListener('click',()=>{
-    cartNumbers(products[i]);
-  });
-
-}
-function cartNumbers(product){
-  console.log(product);
-
-  let productNumbers=localStorage.getItem('cartNumbers');
-  productNumbers=parseInt(productNumbers);
-  if (productNumbers) {
-    localStorage.setItem('cartNumbers',productNumbers+1);
-    document.querySelector('.cart span').textContent=productNumbers+1;
-  }
-  else{
-    localStorage.setItem('cartNumbers',1);
-    document.querySelector('.cart span').textContent=1;
+  function clickHandler(event) {
+    overlay = document.getElementById('overlay').style.display = "none";
   }
 
+  
+
+
+ 
 }
 
 
+let submitForm = document.getElementById('fullform-ceremony');
+submitForm.addEventListener('submit', clickHandler);
 
-function onLoadCartNumbers(){
-  let productNumbers=localStorage.getItem('cartNumbers');
-  if (productNumbers) {
-    document.querySelector('.cart span').textContent=productNumbers;
-  }
+let firstNameValue;
+let lastNameValue;
+let cityForm;
+let occasionForm;
+
+function clickHandler(event) {
+  event.preventDefault();
+  firstNameValue = event.target.firstname.value;
+  lastNameValue = event.target.lastname.value;
+  cityForm = event.target.city.value;
+  occasionForm = event.target.subject.value;
+
+
+  console.log(event.target.firstname.value);
+
+  let Data = {
+
+    firstName : firstNameValue,
+    lastName: lastNameValue,
+    city: cityForm,
+    occasion: occasionForm
+
+ };
+
+  localStorage.setItem("UserForm", JSON.stringify(Data));
+
+
+  renderOverlay();
 }
-onLoadCartNumbers();
-=======
-'use srtice'
->>>>>>> b7041d98bdcf6caa19b2c0bf861616c5d9fcdb43
 
+// -----------------------------------------------------------------------------
